04c000058db50e216319b845616f4e428f3e5ad2
caameuriel <caameuriel@gmail.com>
2021-12-29 11:21:13 -0600

    Revision

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-import { log } from 'console';
 import { UsuarioModel } from 'src/app/models/usuario.model';
 
 @Component({
